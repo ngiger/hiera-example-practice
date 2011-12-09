7387cd8f3704086ab00a4d8747d788672dda4fa4
Niklaus Giger <niklaus.giger@member.fsf.org>
2011-12-09 21:35:58 +0100

    Now it works

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,24 +1,25 @@
 # see also http://www.puppetcookbook.com/posts/create-home-directory-for-managed-users.html
-user { "niklaus":
-   groups => ['adm','x2go']
-   commend => 'This user was created by Puppet',
-   ensure => 'present',
-   managed_home => 'true',
 
+$adminUser = 'niklaus'
+
+user { $adminUser:
+   groups => ['adm','dialout'],
+   comment => 'This user was created by Puppet',
+   ensure => 'present',
+   managehome => 'true',
 } 
+
 node default {
     include  x2go
     include  x2go::client
   include sudo
   include sudo::install
-  include elexis-client
 
-  file{"/tmp/
     include  x2go::client
     include postgres::client
     include postgres::server
-      postgres::database { "elexis-psql": ensure => present,
-      owner => 'niklaus'
-    }
+    postgres::database { "elexis": ensure => present,
+      owner => adminUser
+      }
 }
 
