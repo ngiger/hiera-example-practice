4ed411dd2e006f54617294806648fc644a995f29
Niklaus Giger <niklaus.giger@member.fsf.org>
2011-12-09 23:53:53 +0100

    Added vim, x2go optional

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,12 @@
-# see also http://www.puppetcookbook.com/posts/create-home-directory-for-managed-users.html
+# Damit das Ganze schnell geht und der Platzbedarf minimiert wird,
+# wird nur eine PostgreSQL-DB aufgesetzt.
+# Wenn man mehr will, die x2go-Zeilen ausklammern
+
+
+# Konfigurationsdateien (Kleines Beispiel)
 
 $adminUser = 'niklaus'
+$dbname    = 'elexis'
 
 user { $adminUser:
    groups => ['adm','dialout'],
@@ -10,12 +16,9 @@ user { $adminUser:
 } 
 
 node default {
-    include  x2go
-    include  x2go::client
-  include sudo
-  include sudo::install
-
-    include  x2go::client
+#    include  x2go::client
+#    include  x2go::server
+    include sudo::install
     include postgres::client
     include postgres::server
     postgres::database { "elexis": ensure => present,
@@ -23,3 +26,17 @@ node default {
       }
 }
 
+# Niklaus will, dass immer VIM installiert und als default editor verwendet wird
+
+package { vim:
+        ensure => installed,
+}
+define check_alternatives($linkto) {
+  exec { "/usr/sbin/update-alternatives --set $name $linkto":
+    unless => "/bin/sh -c '[ -L /etc/alternatives/$name ] && [ /etc/alternatives/$name -ef $linkto ]'"
+  }
+}
+
+check_alternatives { "editor":
+  linkto => "/usr/bin/vim.basic"
+}
